321e8a7489704d9ad4fec994e737ef124fc8297a
ygroxie <38132498+ygroxie@users.noreply.github.com>
2018-11-22 02:56:59 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ if (roles.length !== reactions.length) throw "Roles list and reactions list are 
 function generateMessages(){
     var messages = [];
     messages.push(initialMessage);
-    for (let role of roles) messages.push(`:white_check_mark: {role} `); //DONT CHANGE THIS
+    for (let role of roles) messages.push(`React below to get the **"${role}"** role!`); //DONT CHANGE THIS
     return messages;
 }
 
